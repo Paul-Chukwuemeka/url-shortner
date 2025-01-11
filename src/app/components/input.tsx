95a09b8d3e53e9d32ac242fbcbdcf26d8a992f95
Paul Chukwuemeka <paulchukwuemeka123@gmail.com>
2025-01-11 09:37:35 +0100

    Add URL shortening functionality with loading and error handling; integrate toast notifications

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,14 +1,68 @@
 "use client";
 import React from "react";
 import { useState } from "react";
+import { FaCopy } from "react-icons/fa";
 import { IoLinkSharp } from "react-icons/io5";
+import { FadeLoader } from "react-spinners";
+import {
+  ToastContainer,
+  toast,
+  Bounce,
+} from "react-toastify";
+
 const Input = () => {
   const [url, setUrl] = useState<string>("");
+  const [error, setError] =
+    useState<boolean>(false);
+  const [textToCopy, setTextToCopy] =
+    useState<string>("");
+  const [loading, setLoading] =
+    useState<boolean>(false);
+  const [tinyUrl, setTinyUrl] =
+    useState<string>("");
 
-  const shortenUrl = async (url : string) => {
-    const apiKey = url
-    console.log(apiKey)
+  const shortenUrl = async (url: string) => {
+    setLoading(true);
+    setError(false);
+    if (!url) {
+      setError(true);
+      setLoading(false);
+      return;
+    }
+    try {
+      const apiKey =
+        "xg4a692WDt7ueNrnaOjNxITVPKOaTktBaNcpWIq3chPqkFhbhS1yCuZeS9xs";
+      const apiUrl =
+        "https://api.tinyurl.com/create";
+
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          url,
+        }),
+      });
+      const data = await response.json();
+      setTinyUrl(data.data.tiny_url);
+      setTextToCopy(data.data.tiny_url);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+      setLoading(false);
+    }
   };
+
+  const notify = () => toast("Copied");
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(textToCopy);
+    notify();
+  };
+
   return (
     <div
       className="flex flex-col p-6 justify-center items-center
@@ -41,6 +95,43 @@ const Input = () => {
       >
         Shorten Url
       </button>
+
+      {tinyUrl && (
+        <div className="p-5 bg-sky-200 flex items-center justify-center rounded-lg w-full text-center">
+          {loading ? (
+            <FadeLoader
+              color="#38bdf8"
+              height={12}
+              width={5}
+            />
+          ) : (
+            <div className="flex items-center gap-2 text-xl ">
+              <p className="w-96">{tinyUrl}</p>
+              <button onClick={handleCopy}>
+                <FaCopy />
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+      {error && (
+        <div className="p-5 bg-red-300 rounded-lg w-full text-center">
+          An error occurred. Please try again.
+        </div>
+      )}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </div>
   );
 };
