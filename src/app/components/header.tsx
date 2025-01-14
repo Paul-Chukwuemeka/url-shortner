cfee95186ede673466168e1ad36724500165ece2
Paul Chukwuemeka <paulchukwuemeka123@gmail.com>
2025-01-14 12:13:08 +0100

    Refactor Header component to use FiMoon icon and simplify dark mode toggle logic

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,5 @@
 import { FaRegSun } from "react-icons/fa6";
-import { FaMoon } from "react-icons/fa6";
+import { FiMoon } from "react-icons/fi";
 interface HeaderProps {
   darkMode: boolean;
   setDarkMode: (value: boolean) => void;
@@ -12,20 +12,14 @@ const Header: React.FC<HeaderProps> = ({
     <div className="p-4 relative z-10 flex items-center justify-between bg-transparent text-3xl gap-2 w-full max-lg:items-center mb-10">
       <h1 className="gradient-text">LinkMe</h1>
       <div
-        className="relative w-fit gap-2 border overflow-hidden backdrop-blur-lg p-2 text-xl font-bold rounded-3xl flex items-center
+        className=" w-fit overflow-hidden backdrop-blur-lg p-2 text-xl font-bold rounded-3xl flex items-center
       
       "
       >
-        <div className={`absolute duration-200 ${darkMode ? `dark ` : "light"}`}></div>
-        <button className="flex items-center  gap-1 focus:border-none focus:outline-none focus:ring-0"
-        onClick={() => setDarkMode(false)}
+        <button className={`flex items-center border-2 rounded-full p-2 focus:outline-none focus:ring-0 ${darkMode ? `border-white`: `border-black`}`}
+        onClick={() => setDarkMode(!darkMode)}
         >
-          <FaRegSun /> Light
-        </button>
-        <button className="flex items-center gap-1 focus:border-none focus:outline-none focus:ring-0"
-        onClick={() => setDarkMode(true)}
-        >
-          <FaMoon /> Dark
+         { darkMode ? <FiMoon /> : <FaRegSun /> }
         </button>
       </div>
     </div>
